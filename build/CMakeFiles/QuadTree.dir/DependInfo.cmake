cb42be6b52b23e848fa8d15fc519ff6852bd5e9e
Hassan-Bahrami <sthassanbahrami@yahoo..com>
2024-06-15 21:20:10 +1000

    Initial commit

---
+++ build/CMakeFiles/QuadTree.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hassan/Projects/ISTA/QuadTree/src/Circle.cpp" "CMakeFiles/QuadTree.dir/src/Circle.cpp.o" "gcc" "CMakeFiles/QuadTree.dir/src/Circle.cpp.o.d"
+  "/home/hassan/Projects/ISTA/QuadTree/src/Point.cpp" "CMakeFiles/QuadTree.dir/src/Point.cpp.o" "gcc" "CMakeFiles/QuadTree.dir/src/Point.cpp.o.d"
+  "/home/hassan/Projects/ISTA/QuadTree/src/QuadTree.cpp" "CMakeFiles/QuadTree.dir/src/QuadTree.cpp.o" "gcc" "CMakeFiles/QuadTree.dir/src/QuadTree.cpp.o.d"
+  "/home/hassan/Projects/ISTA/QuadTree/src/Rectangle.cpp" "CMakeFiles/QuadTree.dir/src/Rectangle.cpp.o" "gcc" "CMakeFiles/QuadTree.dir/src/Rectangle.cpp.o.d"
+  "/home/hassan/Projects/ISTA/QuadTree/src/main.cpp" "CMakeFiles/QuadTree.dir/src/main.cpp.o" "gcc" "CMakeFiles/QuadTree.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/QuadTree.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuadTree.dir/src/Circle.cpp.o"
+  "CMakeFiles/QuadTree.dir/src/Circle.cpp.o.d"
+  "CMakeFiles/QuadTree.dir/src/Point.cpp.o"
+  "CMakeFiles/QuadTree.dir/src/Point.cpp.o.d"
+  "CMakeFiles/QuadTree.dir/src/QuadTree.cpp.o"
+  "CMakeFiles/QuadTree.dir/src/QuadTree.cpp.o.d"
+  "CMakeFiles/QuadTree.dir/src/Rectangle.cpp.o"
+  "CMakeFiles/QuadTree.dir/src/Rectangle.cpp.o.d"
+  "CMakeFiles/QuadTree.dir/src/main.cpp.o"
+  "CMakeFiles/QuadTree.dir/src/main.cpp.o.d"
+  "QuadTree"
+  "QuadTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuadTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
